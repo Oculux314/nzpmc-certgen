93c1be750087da0cd80048abda8ad59fe3f941d6
Oculux <nate.williamson.nz@gmail.com>
2024-06-29 22:48:27 +1200

    :bento: read in template and eventticket files

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log(this)
+import fs from "fs";
+import puppeteer from "puppeteer";
+
+const TEMPLATE_FILE = "src/template.html"; // An html certificate template
+const EVENTTICKET_FILE = "src/registration.eventTicket.json"; // Download this query from Mongo (eventTickets): {"_id.eventCode": "NZPMC24R1"}
+
+const template = fs.readFileSync(TEMPLATE_FILE, "utf8");
+const eventTickets = JSON.parse(fs.readFileSync(EVENTTICKET_FILE, "utf8"));
+console.log(eventTicket);
