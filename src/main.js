9df44ac1e114ea9bfe7af5d2e443c30897bd8daf
Oculux <nate.williamson.nz@gmail.com>
2024-06-29 23:41:11 +1200

    :sparkles: implement html to pdf

--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
 import fs from "fs";
+import path from "path";
 import puppeteer from "puppeteer";
 
 const TEMPLATE_FILE = "src/template.html"; // An html certificate template
 const EVENTTICKET_FILE = "src/registration.eventTicket.json"; // Download this query from Mongo (eventTickets): {"_id.eventCode": "NZPMC24R1"}
+const OUT_DIR = "out"; // Output directory for pdf certificates
+const REPLACEMENTS = {};
+const LANDSCAPE = true;
+const SCALE = 1;
 
 const template = fs.readFileSync(TEMPLATE_FILE, "utf8");
 const eventTickets = JSON.parse(fs.readFileSync(EVENTTICKET_FILE, "utf8"));
-console.log(eventTicket);
+
+async function generatePdf(html, filename, isLandscape = false, scale = 1) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setContent(html);
+  await page.pdf({
+    path: filename,
+    format: "A4",
+    landscape: isLandscape,
+    printBackground: true,
+    scale,
+  });
+}
+
+fs.mkdir(OUT_DIR, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+generatePdf(template, path.join(OUT_DIR, "certificate.pdf"), LANDSCAPE, SCALE);
